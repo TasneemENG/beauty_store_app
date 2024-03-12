3e10e9258ffeeb7e7afc9e910d2a2b77ce3d5528
randa <randaghoniem1@gmail.com>
2024-03-12 12:34:30 +0200

    randa

---
+++ lib/models/material.dart
@@ -0,0 +1,14 @@
+class Material {
+  final String id;
+  final String title;
+
+  final String imageUrl;
+  final String salary;
+
+
+  final String description;
+  final String categoryNumber;
+
+  const Material(
+      {required this .id, required this .title, required this .imageUrl, required this .salary,  required this .description, required this .categoryNumber});
+}
